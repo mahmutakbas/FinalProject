633322c1345d048f98bcbf6f848891fc132841db
mahmut akbaş <mahmut-akbas02@hotmail.com>
2021-02-13 23:59:44 +0300

    13.02.2021 Ders Sonu bağımlılık nasıl azaltira bir giriş yapıldı WebApi post ve get yapılarını gördük

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //[Attribute] ... Javada ise [Annotation] 
+    public class ProductsController : ControllerBase
+    {
+        //Loosly coupled---gevşek bağımlı
+        //naming convention
+        //IoC Container -- Inversion of Control
+        //Belleğe ekleyerek istediğimiz zaman oradan çekebilir hale getiriyoruz
+        IProductService _productService;
+        //Bu adama soyut referans vermemiz gerekiyor
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        //Bir katman diğer katmanın somutuna bağlantı kuramayız
+        //Yarın karşısına ne çıkacağını bilmeyen ama bileerek yazan geliştirici ol
+        //IProductService _productService = new ProductManager(new EfProductDal());
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Swagger --> 
+            //Dependency chain -- bağımlılık var
+            var result = _productService.GetAll();
+            if (result.Success)
+            { //200 dönderecek get request 200 ile çalışır
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int productId)
+        {
+            var result = _productService.GetById(productId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        //silme ve güncellemeler için post kullanılır ama
+        //silme için httpdelete kullanabiliriz
+        //güncelleme için httpput kullanabiliriz
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,57 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //AOP için Autofac kullanacaðýz
+            //Autofac,Ninject,CastleWindsor,StructureMap,LightInject,DryInject ---> IoC container
+            //AOP
+            services.AddControllers();
+            //Bir baðýmlýlýk görürsen onu al
+            //IProductService görürsen  ProductManager arka planda ona ver
+            //singleton içinde data tutmuyorsak yapmalýyýz.Örneðin sepet birbirine girebilir
+            services.AddSingleton<IProductService, ProductManager>();
+            services.AddSingleton<IProductDal, EfProductDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
